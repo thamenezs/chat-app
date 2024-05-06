ebc491396d0e13ad4f539b5cadb0f5c58b5321d6
Thais Menezes <thais.mas@hotmail.com>
2024-05-06 20:50:29 -0300

    chore: creating project

---
+++ FormulaOne.ChatService/DataService/SharedDb.cs
@@ -0,0 +1,12 @@
+﻿using FormulaOne.ChatService.Models;
+using System.Collections.Concurrent;
+
+namespace FormulaOne.ChatService.DataService
+{
+    public class SharedDb
+    {
+        private readonly ConcurrentDictionary<string, UserConnection> _connections = new();
+
+        public ConcurrentDictionary<string, UserConnection> connections => _connections;
+    }
+}
---
+++ FormulaOne.ChatService/Hubs/ChatHub.cs
@@ -0,0 +1,42 @@
+﻿using FormulaOne.ChatService.DataService;
+using FormulaOne.ChatService.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace FormulaOne.ChatService.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        private readonly SharedDb _shared;
+
+        public ChatHub(SharedDb shared) => _shared = shared;
+
+        public async Task JoinChat(UserConnection conn)
+        {
+            await Clients.All
+                .SendAsync("ReceiveMessage", "admin", $"{conn.Username} has joined");
+        }
+
+        public async Task JoinSpecificChatRoom(UserConnection conn)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, conn.ChatRoom);
+            _shared.connections[Context.ConnectionId] = conn;
+            await Clients.Group(conn.ChatRoom).SendAsync("JoinSpecificChatRoom", "admin", $"{conn.Username} has joined the {conn.ChatRoom}");
+
+        }
+
+        public async Task SendMessage(string msg)
+        {
+            if(_shared.connections.TryGetValue(Context.ConnectionId, out UserConnection conn)) 
+            {
+                await Clients.Group(conn.ChatRoom).SendAsync("ReceiveSpecificMessage", conn.Username, msg);
+;            }
+        }
+
+        //public async Task LeaveChat(UserConnection conn)
+        //{
+        //    await Groups.RemoveFromGroupAsync(Context.ConnectionId, conn.ChatRoom);
+        //    await Clients.Group(conn.ChatRoom).SendAsync("LeaveChatRoom", "admin", $"{conn.Username} has left the room");
+        //}
+    }
+}
